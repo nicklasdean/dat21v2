2a7d04e36d479a7bbd1b1c891868a2c5ffc95347
dean <nifr@kea.dk>
2021-02-09 09:55:38 +0100

    Corrected comments

--- src/variables_operators/OperatorExample.java
+++ src/variables_operators/OperatorExample.java
@@ -2,6 +2,6 @@ package variables_operators;
 
 public class OperatorExample {
     public static void main(String[] args) {
-        int sum = 2 + 5; // Expression of operands & operators, summing two numbers and changing their variable
+        int sum = 2 + 5; // Expression of operands & operator, summing two numbers and changing their variable
     }
 }
--- src/variables_operators/VariableExample.java
+++ src/variables_operators/VariableExample.java
@@ -4,7 +4,7 @@ public class VariableExample {
     public static void main(String[] args) {
 
         int theLoneliestNumberSinceTheNumberOne = 2; //Storing the value 2
-        double twoPointFive = 2.0; //Storing the value 2.5
+        double twoPointFive = 2.5; //Storing the value 2.5
         char theCharacterC = 'c'; //Storing the letter c
 
         System.out.println(theLoneliestNumberSinceTheNumberOne);
