ffcbbb4aee7d8b642a827b7c06b595981e1dfb3f
dean <nifr@kea.dk>
2021-02-10 13:15:46 +0100

    Final naming convention

--- src/input_output/FinalExample.java
+++ src/input_output/FinalExample.java
@@ -3,6 +3,6 @@ package input_output;
 public class FinalExample {
     public static void main(String[] args) {
         //A mile is 1.609 kilometers Compute the amount of miles corresponding to 19 kilometers, based on user input.
-        final double kmPrMile = 1.609;
+        final double KM_PR_MILE = 1.609;
     }
 }
